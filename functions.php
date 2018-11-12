6acd88aa88f3bf82bcea1f7a6f2ae410677d95f7
Marc <ott11marc@gmail.com>
2018-11-12 16:17:32 +0100

    Base (header)

--- functions.php
+++ functions.php
@@ -9,8 +9,42 @@ define( 'JS_URL' ,              THEME_URL .    '/assets/scripts'      );
 define( 'FAVICONS_URL' ,        THEME_URL .    '/assets/favicon'      );
 define( 'ADMIN_IMAGES_URL' ,    IMAGES_URL .   '/admin'             );
 
+define('LGMAC_VERSION','1.0.2');
 
-foreach ( glob( THEME_PATH . "/inc/*.php" ) as $file ) {
-    include_once $file;
+function lgmac_scripts(){
+    wp_enqueue_style('lgmac_bootstrap-core',get_template_directory_uri() . '/css/bootstrap.min.css',array(),LGMAC_VERSION,'all');
+    wp_enqueue_style('lgmac_custom',get_template_directory_uri() . '/style.css',array('lgmac_bootstrap-core'),LGMAC_VERSION,'all');
+
+    
+    wp_enqueue_script('bootstrap-js',get_template_directory_uri() . '/js/bootstrap.min.js',array(jquery), LGMAC_VERSION , true );
+    wp_enqueue_script('lgmac_admin_script',get_template_directory_uri() . '/js/script.js',array('jquery','bootstrap-js'),LGMAC_VERSION,true);
 }
+
+add_action('wp_enqueue_scripts','lgmac_scripts');
+
+function lgmac_admin_scripts(){
+    wp_enqueue_style('bootstrap-adm-core', get_template_directory_uri() . '/css/bootstrap.min.css',array(),LGMAC_VERSION);
+
+}
+add_action('admin_init','lgmac_admin_scripts');
+
+
+
+function lgmac_setup(){
+
+    add_theme_support('post-thumbnails');
+
+    remove_action('wp_gead','wp_generator');
+
+    remove_filter('the_content','wptexturize');
+
+    add_theme_support('title-tag');
+
+    register_nav_menus(array('primary' => 'principal') );
+
+    
+}
+
+add_action('after_setup_theme','lgmac_setup');
+
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,52 @@
 <?php get_header(); ?>
 
-<?php get_footer(); ?>
+
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Coucou c'est nous</h1>
+        </div>
+    </div>
+
+    <?php 
+
+        wp_nav_menu( array(
+            'menu'              => 'top-menu',
+            'theme_location'    => 'primary'
+            )
+        );
+
+    ?>
+
+    <section>
+        <div class="container">
+            <?php if (have_posts()): ?>
+            
+
+                <?php while(have_posts()): the_post(); ?>
+
+                    <div class="row m-dw-30">
+                        <div class="col-xs-2">
+                            <?php the_post_thumbnail('thumbnail');?>
+                        </div>
+                        <div class="col-xs-10">
+                            <h1> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a> </h1>
+                            <?php the_excerpt(); ?>
+                        </div>
+                    </div>
+                    
+                <?php endwhile; ?>
+        
+            <?php else: ?>            
+                <div class="row m-dw-30">                    
+                    <div class="col-xs-12">                        
+                        <p>Il n'y a pas de résultat</p>
+                    </div>
+                </div>
+                
+
+            <?php endif; ?>
+        </div>
+    </section>
+
+
+<?php get_footer(); ?> 
